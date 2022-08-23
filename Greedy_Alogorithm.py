bca21aa4c9003d299a41551531f8b96a7589fe0c
magee11 <65508144+magee11@users.noreply.github.com>
2022-08-23 15:21:50 +0530

    Update Greedy_Alogorithm.py

--- Greedy_Alogorithm.py
+++ Greedy_Alogorithm.py
@@ -1,20 +1,11 @@
-def Fgreedy(cash):
-    posibility = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
-    n = len(posibility)
-    final = []
-    i = n - 1
-    while i >= 0:
-        while cash >= posibility[i]:
-            cash = cash - posibility[i]
-            final.append(posibility[i])
-        i -= 1
-
-    for i in range(len(final)):
-        print(final[i], end="\n")
-
-
-if __name__ == '__main__':
-
-    print('Enter the cash : ')
-    n = int(input())
-    Fgreedy(n)
+a=[1,2,5]
+b=11
+count=0
+d=[]
+while b!=0:
+    if b>=a[-1]:
+        b=b-a[-1]
+        count=count+1
+    if b<a[-1]:
+        d.append(a.pop())
+print(count)
